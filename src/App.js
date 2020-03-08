6b318495fbd07ac95c0baf844204c5c2d49c55c6
Andrés A. Acevedo <andy_xz@live.com>
2020-03-08 19:47:53 -0400

    testing fetching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log('hey');
+
+    axios
+      .get(`https://parseapi.back4app.com/classes/Comment`, {
+        headers: {
+          'X-Parse-Application-Id': process.env.REACT_APP_APP_ID,
+          'X-Parse-REST-API-Key': process.env.REACT_APP_API_KEY,
+          'X-Parse-Master-Key': process.env.REACT_APP_MASTER_KEY,
+        },
+        params: {
+          limit: '100000000',
+        },
+      })
+      .then((e) => {
+        console.log('e', e);
+        // this.setState({ list: e.data.results });
+      })
+      .catch((err) => console.log('in the error', err));
+
+    // axios({
+    //   method: 'GET',
+    //   url: `https://parseapi.back4app.com/classes/_User/`,
+    //   headers: {
+    //     'X-Parse-Application-Id': process.env.REACT_APP_APP_ID,
+    //     'X-Parse-REST-API-Key': process.env.REACT_APP_API_KEY,
+    //     'X-Parse-Master-Key': process.env.REACT_APP_MASTER_KEY,
+    //     'Content-Type': 'application/json',
+    //   },
+    // })
+    //   .then((e) => {
+    //     console.log('e', e);
+    //     // this.setState({
+    //     //   open: false,
+    //     // });
+    //     // window.location.reload();
+    //   })
+    //   .catch((err) => console.log(err.response));
+    // Update the document title using the browser API
+  });
+
   return (
     <div className="App">
       <header className="App-header">
