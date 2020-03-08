bd871ecdf525399071792d7afccdcfbe13842a8b
Andrés A. Acevedo <andy_xz@live.com>
2020-03-08 19:09:59 -0400

    Updates

---
+++ precache-manifest.166f090b0a4bf2c3fd520e63b683dcfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3879cef5b9327c9d1312e8dbd19ef64",
+    "url": "/tickit/index.html"
+  },
+  {
+    "revision": "a5c43db30d76fc5a78de",
+    "url": "/tickit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "568be069525a34797cad",
+    "url": "/tickit/static/js/2.a493c800.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tickit/static/js/2.a493c800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c43db30d76fc5a78de",
+    "url": "/tickit/static/js/main.7513220a.chunk.js"
+  },
+  {
+    "revision": "83338978dbf69749d41b",
+    "url": "/tickit/static/js/runtime-main.ca40f2ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tickit/static/media/logo.5d5d9eef.svg"
+  }
+]);
